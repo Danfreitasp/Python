cc98a92978e149446383d08f049581a2e75c27ff
Daniel Pinto <164277441+Danfreitasp@users.noreply.github.com>
2025-09-16 19:04:52 -0300

    Update helloWorld.py

--- helloWorld.py
+++ helloWorld.py
@@ -3,5 +3,7 @@ idade = input("Digite sua idade: ")
 peso = input("Digite seu peso: ")
 altura = input("Digite sua altura: ")
 
+print("Olá mundo!")
 print(f"Olá, {nome}! Você tem {idade} anos, pesa {peso} kg e mede {altura} metros.")
+
 
