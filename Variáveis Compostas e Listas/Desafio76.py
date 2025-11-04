e9270ff3d3ebf4d1908bb697559545000459f9ff
Danfreitasp <danfreitasp@gmail.com>
2025-11-03 22:04:06 -0300

    Adicionando codigos dos desafios 76 e 77

---
+++ Variáveis Compostas e Listas/Desafio76.py
@@ -0,0 +1,8 @@
+# Desafio 077 - Programa que faz uma listagem itens com preços tabulados.
+itens = ('Pao', 'R$ 0.50', 'Leite', 'R$ 2.50', 'Café', 'R$ 2.00')
+print("-" * 30)
+print('{:^30}'.format("Listagem de Preços:"))
+print("-" * 30)
+print(itens[0], "-" * 30, itens[1])
+print(itens[2], "-" * 30, itens[3])
+print(itens[4], "-" * 30, itens[5])
---
+++ Variáveis Compostas e Listas/Desafio77.py
@@ -0,0 +1,2 @@
+# Desafio 077 - Programa que lê uma tupla e mostra as vogais.
+palavras = ('cachorro', 'camisa', 'caixa', 'mochila', 'caneta', 'sapato', 'celular')
