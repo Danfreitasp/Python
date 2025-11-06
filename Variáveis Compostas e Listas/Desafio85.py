06b95b814c4c3a799251a3af9dd186a9a97faf89
Danfreitasp <danfreitasp@gmail.com>
2025-11-05 22:17:06 -0300

    Ajustando Desafio 85

--- Variáveis Compostas e Listas/Desafio85.py
+++ Variáveis Compostas e Listas/Desafio85.py
@@ -1,4 +1,4 @@
-# Programa que pede 7 valores, cadastra em uma lista única separando números pares de ímpares; No final mostra os valores pares e ímpares em ordem crescente.
+# Desafio 085 - Programa que pede 7 valores, cadastra em uma lista única separando números pares de ímpares; No final mostra os valores pares e ímpares em ordem crescente.
 numeros = [[], []]
 for n in range(0, 7):
     num = int(input('Digite um número: '))
